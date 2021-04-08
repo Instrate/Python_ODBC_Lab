70ba78d6f729ea477cf9c5a324d2f1b231bfbd3c
Instrate <Instrate@users.noreply.github.com>
2021-04-08 23:07:37 +0300

    -test

--- main.py
+++ main.py
@@ -7,17 +7,28 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import *
 
 
-class User(QWidget,):
-    def __init__():
-        super().__init__()
-        self.setupUi(self)
+# class User(QWidget,):
+#     def __init__():
+#         super().__init__()
+#         self.setupUi(self)
 
 
 def main():
-    app = QApplication(sys.argv)
-    window = userSpace()
-    window.show()
-    app.exec_()  
+    # app = QApplication(sys.argv)
+    # window = userSpace()
+    # window.show()
+    # app.exec_()  
+    #MySQL ODBC 8.0 Unicode Driver
+    #SQL Server
+    con = pyodbc.connect('DRIVER={MySQL ODBC 8.0 Unicode Driver};User ID=root;Password=Bigben00;Server=localhost;Database=car_showroom;Port=3306;String Types=Unicode')
+    cursor = con.cursor()
+    request = 'SELECT * FROM `Склад`'
+    cursor.execute(request)
+
+    for row in cursor:
+        print(row)
+
+
 
 if (__name__=="__main__"):
     main()
