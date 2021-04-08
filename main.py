e67fd54408ad406489bb893c84e0531601c0554e
Instrate <Instrate@users.noreply.github.com>
2021-04-09 00:06:17 +0300

    -template 0.00001

--- main.py
+++ main.py
@@ -8,27 +8,24 @@ from PyQt5.QtGui import *
 
 import auth
 
-# class User(QWidget,):
-#     def __init__():
-#         super().__init__()
-#         self.setupUi(self)
+class User(QMainWindow,auth.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 
 def main():
-    # app = QApplication(sys.argv)
-    # window = userSpace()
-    # window.show()
-    # app.exec_()  
-    #MySQL ODBC 8.0 Unicode Driver
-    #SQL Server
-    #ODBC Driver 17 for SQL Server
-    con = pyodbc.connect('DRIVER={MySQL ODBC 8.0 Unicode Driver};User ID=ODBC;Password=Bigben00;Server=localhost;Port=3306;Database=car_showroom;String Types=Unicode')
-    cursor = con.cursor()
-    request = 'SELECT * FROM `Склад`'
-    cursor.execute(request)
+    app = QApplication(sys.argv)
+    window = User()
+    window.show()
+    app.exec_()  
+    # con = pyodbc.connect('DRIVER={MySQL ODBC 8.0 Unicode Driver};User ID=ODBC;Password=Bigben00;Server=localhost;Port=3306;Database=car_showroom;String Types=Unicode')
+    # cursor = con.cursor()
+    # request = 'SELECT * FROM `Склад`'
+    # cursor.execute(request)
 
-    for row in cursor:
-        print(row)
+    # for row in cursor:
+    #     print(row)
 
 
 
